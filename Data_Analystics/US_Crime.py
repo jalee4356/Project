0747714a675bc3ff64cafdef49fcca163052fb2c
jalee4356 <ssgyung02@gmail.com>
2023-12-24 18:37:06 +0900

    add US_Crime

---
+++ Data_Analystics/US_Crime.py
@@ -0,0 +1,17 @@
+import pandas as pd
+#import csv
+#import matplotlib.pyplot as plt
+from plotnine import ggplot, aes, geom_line
+
+# download data
+url = 'http://www.statsci.org/data/general/uscrime.txt'
+df = pd.read_csv(url, delimiter = '\t')
+
+# change dataframe to csvfile
+df.to_csv('data.csv', index=False)
+
+#plot data using ggplot library
+data = pd.read_csv('data.csv')
+
+gg = (ggplot(data, aes(x='Time', y='Crime')) + geom_line())
+print(gg)
