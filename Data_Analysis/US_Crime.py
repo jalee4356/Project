65c5ea33939af9efe8244c29aeb54fb6580f5ff1
jalee4356 <ssgyung02@gmail.com>
2023-12-28 14:08:50 +0900

    edit files

--- Data_Analysis/US_Crime.py
+++ Data_Analysis/US_Crime.py
@@ -1,15 +1,38 @@
 import pandas as pd
-from plotnine import ggplot, aes, geom_line
+from plotnine import ggplot, aes, geom_point, stat_smooth
 
-# download data
+# url for csv data
 url = 'http://www.statsci.org/data/general/uscrime.txt'
-df = pd.read_csv(url, delimiter = '\t')
 
-# change dataframe to csvfile
-df.to_csv('data.csv', index=False)
+class BasicAnalysis:
+    
+    def __init__(self):
+        self.csv_data = 'data.csv'
+        return
+    
+    def download_to_csv(self, url):
+        # download data
+        df = pd.read_csv(url, delimiter = '\t')
 
-#plot data using ggplot library
-data = pd.read_csv('data.csv')
-
-gg = (ggplot(data, aes(x='Time', y='Crime')) + geom_line())
-print(gg)
+        # save dataframe to csvfile
+        df.to_csv(self.csv_data, index=False)
+        
+        return
+    
+    def scatter_plot(self):
+        #load the data to a df
+        data = pd.read_csv(self.csv_data)
+        
+        for column in data.columns:
+            print(column)
+            gg = (ggplot(data, aes(x= data[column], y='Crime')) + geom_point() + stat_smooth(method = 'lm'))
+            plot_name = column + ".jpg"
+            #save plots in plots directory
+            gg.save(filename = plot_name, path = "Plot images")
+            
+        return
+    
+if __name__ == "__main__":
+    ba = BasicAnalysis()
+    ba.download_to_csv(url)
+    ba.scatter_plot()
