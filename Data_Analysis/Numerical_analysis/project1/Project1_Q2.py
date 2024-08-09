af140d8f02f23af3a42fa02dd8590ee414aac300
jalee4356 <ssgyung02@gmail.com>
2024-08-09 16:24:01 +0900

    edit numerical analysis project

--- Data_Analysis/Numerical_analysis/project1/Project1_Q2.py
+++ Data_Analysis/Numerical_analysis/project1/Project1_Q2.py
@@ -1,8 +1,15 @@
-## 2번
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  7 12:22:09 2024
 
-import numpy as np
+@author: Eve
+"""
+from datetime import datetime
+import os
 import matplotlib.pyplot as plt
+import numpy as np
 import math
+
 
 class RootEstimator:
 
@@ -28,43 +35,59 @@ class RootEstimator:
     def plot_f_x(self):
         x = np.arange(self.a - 1, self.c + 1.1, 0.01)
         y = [self.f(x_i) for x_i in x]
-        title = f'f(x) = (x - {self.a})(x - {self.b})(x - {self.c})'
-        plt.plot(x, y)
-        plt.title(title)
+        title = "Plot for f(x) = (x - %s) * (x - %s) * (x - %s)\n" %(self.a, self.b, self.c) 
+        fig, ax = plt.subplots(figsize=(8, 8))
+        ax.plot(x, y)
+        ax.set_title(title, fontsize = 16)
+        ax.set_xlabel('x', fontsize = 16)
+        ax.set_ylabel('f(x)', fontsize = 16)
+        ax.axhline(y=0, color='k')
+        ax.axvline(x=0, color='k')
         plt.grid()
         plt.show()
+        
+        if not os.path.exists("Q1"):
+            os.makedirs("Q1")
+        path = os.path.join("Q1", "Plot_f(x)=(x-%s)(x-%s)(x-%s)" %(self.a, self.b, self.c))
+        fig.savefig(path)
+        plt.close()
 
         return
 
-    def bisection_method(self, xl, xu):
-      n_i = 0
-      if self.f(xl) * self.f(xu) >= 0:
-        print("There's no roots")
+    def bisection_method(self, xl, xu, f):       
+        n_i = 0
+        if self.f(xl) * self.f(xu) >= 0:
+            #print("There's no roots")
+            f.write("There's no roots\n")
+            return
+
+        while xu - xl > self.eps:
+            n_i += 1
+            xr = (xl + xu) / 2
+            D = self.f(xl) * self.f(xr)
+
+            if D <= 0:
+                xu = xr
+            elif D > 0:
+                xl = xr
+
+            Et = np.abs((xu - xl) / xu)
+
+            if n_i >=  2:
+                Ea = np.abs((xr - (xl+xu)/2) / xr)
+            else: Ea = np.nan
+
+        
+            f.write(f'n = {n_i} --> xl : {xl:.4f}, xu: {xu:.4f}, xr: {(xl + xu) / 2:4f}, Ea: {Ea:4f}%, error: {Et:.4f}%\n')
+            #print(f'n = {n_i} --> xl : {xl:.4f}, xu: {xu:.4f}, xr: {(xl + xu) / 2:4f}, Ea: {Ea:4f}%, error: {Et:.4f}%')
+
         return
 
-      while xu - xl > self.eps:
-        n_i += 1
-        xr = (xl + xu) / 2
-        D = self.f(xl) * self.f(xr)
-
-        if D <= 0:
-            xu = xr
-        elif D > 0:
-            xl = xr
-
-        Et = np.abs((xu - xl) / xu)
-
-        if n_i >=  2:
-          Ea = np.abs((xr - (xl+xu)/2) / xr)
-        else: Ea = np.nan
-
-        print(f'n = {n_i} --> xl : {xl:.4f}, xu: {xu:.4f}, xr: {(xl + xu) / 2:4f}, Ea: {Ea:4f}%, error: {Et:.4f}%')
-
-      return
-
-    def fixedPointIteration_method(self, x_old):
+    def fixedPointIteration_method(self, x_old, f):
       n_i = 0
-
+      
+      title = "Plot fixedPointIteration Result" 
+      fig, ax = plt.subplots(figsize=(8, 8))
       xx = np.arange(self.a - 1, self.c + 1.1, 0.01)
       y1 = xx
       plt.plot(xx, self.g(xx), label="x = g(x)")
@@ -78,18 +101,29 @@ class RootEstimator:
 
         Et = np.abs((x_new - x_old) / x_new) * 100
         ratio = Et / prev_Et
-        print(f'n = {n_i} --> result : {x_new:.4f}, error:{Et:.4f}, ratio = {ratio:.4f}')
+        f.write(f'n = {n_i} --> result : {x_new:.4f}, error:{Et:.4f}, ratio = {ratio:.4f}\n')
+        #print(f'n = {n_i} --> result : {x_new:.4f}, error:{Et:.4f}, ratio = {ratio:.4f}')
         prev_Et = Et
         x_old = x_new
 
         plt.plot(x_new, self.g(x_new), 'm.')
+      ax.set_title(title, fontsize = 16)
+      ax.set_xlabel('x', fontsize = 16)
+      ax.set_ylabel('f(x)', fontsize = 16)
+      ax.axhline(y=0, color='k')
+      ax.axvline(x=0, color='k')
+      plt.grid()
+      path = os.path.join("Q1", "Plot_fixedPointIteration_result")
+      fig.savefig(path)
       plt.show()
 
       return
 
-    def newtonRaphson_method(self, x_old):
+    def newtonRaphson_method(self, x_old, f):
         n_i = 0 # number of iteration
 
+        title = "Plot newtonRaphson Result" 
+        fig, ax = plt.subplots(figsize=(8, 8))
         x = np.arange(self.a - 1, self.c + 1.1, 0.01)
         y = self.f(x)
         plt.plot(x, y)
@@ -103,25 +137,51 @@ class RootEstimator:
             plt.scatter(x_old, self.f(x_old))
 
             Et = np.abs((x_new - x_old) / x_new) * 100
-            print(f'n = {n_i}, x_new = {x_new:.4f}, Error: {Et :4f}')
+            f.write(f'n = {n_i}, x_new = {x_new:.4f}, Error: {Et :4f}\n')
+            #print(f'n = {n_i}, x_new = {x_new:.4f}, Error: {Et :4f}')
             x_old = x_new
-
+        ax.set_title(title, fontsize = 16)
+        ax.set_xlabel('x', fontsize = 16)
+        ax.set_ylabel('f(x)', fontsize = 16)
+        ax.axhline(y=0, color='k')
+        ax.axvline(x=0, color='k')
+        path = os.path.join("Q1", "Plot_newtonRaphson_result")
+        fig.savefig(path)
         plt.show()
 
         return
 
 
 if __name__ == "__main__":
-    # 2 - 1
+    # 2 - 1 and 2 - 2
     a, b, c = 1, 2, 3
     xl = 0.5
     xu = 1.5
     re = RootEstimator(a, b, c)
     re.plot_f_x()
+    # bisection_method
+    path = os.path.join("Q1", "bisection_results.txt")
+    with open(path, 'w') as f:
+        f.write(" Results for %s\n\n" % datetime.now())
+        f.write("< Results for Bisction Method >\n\n")
+        re.bisection_method(xl, xu, f)
+    # fixedPointIteration_method
+    path = os.path.join("Q1", "fixedPointIteration_results.txt")
+    with open(path, 'w') as f:
+        f.write(" Results for %s\n\n" % datetime.now())
+        f.write("< Results for fixedPointIteration Method >\n\n")
+        re.fixedPointIteration_method(xl, f)
+    # newtonRaphson_method
+    path = os.path.join("Q1", "newtonRaphson_results.txt")
+    with open(path, 'w') as f:
+        f.write(" Results for %s\n\n" % datetime.now())
+        f.write("< Results for newtonRaphson Method >\n\n")
+        re.newtonRaphson_method(xl, f)
+        
     # 2 - 2
     print("\n< bisection method >\n")
-    re.bisection_method(xl, xu)
+ #   re.bisection_method(xl, xu)
     print("\n< Simple fixed-point iteration method >\n")
-    re.fixedPointIteration_method(xl)
+ #   re.fixedPointIteration_method(xl)
     print("\n< Newton-Raphson method >\n")
-    re.newtonRaphson_method(xl)
+ #   re.newtonRaphson_method(xl)
