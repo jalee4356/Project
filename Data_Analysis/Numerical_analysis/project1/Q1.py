dff522a671f72a231363d8f1c2c879874fbd6bb6
jalee4356 <ssgyung02@gmail.com>
2024-08-09 21:58:23 +0900

    Q1 update

--- Data_Analysis/Numerical_analysis/project1/Q1.py
+++ Data_Analysis/Numerical_analysis/project1/Q1.py
@@ -1,10 +1,16 @@
-## 1번
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  7 12:22:09 2024
 
-import numpy as np
+@author: Eve
+"""
+from datetime import datetime
+import os
 import matplotlib.pyplot as plt
+import numpy as np
 import math
 
-class sin_taylor_series:
+class SinTaylorSeries:
 
     def f(self, x):
         return np.sin(x)
@@ -24,17 +30,19 @@ class sin_taylor_series:
 
         return approx_sum
 
-    def truncation_error(self, x, a, N):
-
+    def truncation_error(self, x, a, N, f):
         for n in range(N+1):
             approx_sum = self.PN(x, a, n)
 
             Et = np.abs((self.f(x) - approx_sum) / self.f(x)) * 100
-            print(f'N = {n} --> result : {approx_sum:.4f}, error :{Et:.4f}%')
+            f.write(f'N = {n} --> result : {approx_sum:.4f}, error : {Et:.4f}%\n')
+            #print(f'N = {n} --> result : {approx_sum:.4f}, error :{Et:.4f}%')
 
         return
 
     def plot(self, delta):
+        title = "Plot Taylor Series Result" 
+        fig, ax = plt.subplots(figsize=(8, 8))
         x = np.linspace(-np.pi/2, np.pi/2, 1000)
         a = np.pi/6 + delta
         P1 = lambda x: np.sin(a) + np.cos(a) * (x - a)
@@ -47,23 +55,37 @@ class sin_taylor_series:
 
         plt.plot(x, y1, label='P1')
         plt.plot(x, y2, label='P2')
-        plt.plot(x, y4, label='P3')
+        plt.plot(x, y4, label='P4')
         plt.legend()
+        ax.set_title(title, fontsize = 16)
+        ax.set_xlabel('x', fontsize = 16)
+        ax.set_ylabel('y', fontsize = 16)
+        ax.axhline(y=0, color='k')
+        ax.axvline(x=0, color='k')
         plt.grid()
+        path = os.path.join("Q1_Results", "Plot_taylorSeries_result")
+        fig.savefig(path)
         plt.show()
 
         return
 
 if __name__ == "__main__":
-    q1 = sin_taylor_series()
+    if not os.path.exists("Q1_Results"):
+        os.makedirs("Q1_Results")
+        
+    q1 = SinTaylorSeries()
+    delta = 0.3
     # Test the function for sin(x) near pi/6 with N = 7
-    x = np.pi / 6 + 0.1  # Choose a point near pi/6
+    x = np.pi / 6 + delta # Choose a point near pi/6
     N = 7  # Degree of the Taylor polynomial
-    #pproximation = PN(x, N)
-    #print("Approximation of sin(x) with N = 7:", approximation)
-
+    
     # 1 - 2
-    q1.truncation_error(np.pi/6, 0.3, 7)
+    path = os.path.join("Q1_Results", "truncationError_results.txt")
+    with open(path, 'w') as f:
+        f.write(" Results for %s\n\n" % datetime.now())
+        f.write("< Results for truncation error >\n")
+        f.write(f'degree = {N}, delta = {delta}\n\n')
+        q1.truncation_error(np.pi/6, delta, N, f)
 
     # 1 - 3
-    q1.plot(0.3)
+    q1.plot(delta)
